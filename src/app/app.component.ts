f129827870c1618c3a14757f591aef2675668ea7
Mostafa Amin <125675410+moustafaamindev@users.noreply.github.com>
2023-09-07 10:23:54 +0300

    finish project angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import { OwlOptions } from 'ngx-owl-carousel-o';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'Male-Fashion';
+  nextIcon = '<i class="fa-solid fa-arrow-left next_arrow"></i>';
+  prevIcon = '<i class="fa-solid fa-arrow-right prev_arrow"></i>';
+  customOptions: OwlOptions = {
+    loop: true,
+    margin: 0,
+    dots: false,
+    animateOut: 'fadeOut',
+    navText: [this.nextIcon, this.prevIcon],
+    responsive: {
+      0: {
+        items: 1,
+      },
+    },
+    nav: true,
+  };
+
+  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { CarouselModule } from 'ngx-owl-carousel-o';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule,
+    NgbModule,
+    CarouselModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
